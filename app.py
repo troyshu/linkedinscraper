16c7fb49e0848a5c1b72c5f85a9de1c856765faf
Troy Shu <tmshu1@gmail.com>
2014-07-19 15:14:55 -0400

    add application

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ class LIScraper:
     def __init__(self):
         self.authentication = linkedin.LinkedInDeveloperAuthentication("77b5cimkmbwtcx", "p0AZNOb6OSv5zmZe", "1b0ee31e-3e3a-4688-ba37-5171633b112a", "8b295ec8-3c40-4464-82ed-2dc1054a94c2", "http://localhost:8000", linkedin.PERMISSIONS.enums.values())
 
+        self.application = linkedin.LinkedInApplication(self.authentication)
+
     def getAuthenticationObj(self):
         return self.authentication
 
