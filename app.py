f5f1d3d0da88f069bf72d28af7488d20be63fad4
Troy Shu <tmshu1@gmail.com>
2014-07-19 15:00:23 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from linkedin import linkedin
+
+
+class LIScraper:
+
+
+    def init(self):
+        self.authentication = linkedin.LinkedInDeveloperAuthentication("77b5cimkmbwtcx", "p0AZNOb6OSv5zmZe", "1b0ee31e-3e3a-4688-ba37-5171633b112a", "8b295ec8-3c40-4464-82ed-2dc1054a94c2", "http://localhost:8000", linkedin.PERMISSIONS.enums.values())
+
+    def getAuthenticationObj(self):
+        return self.authentication
